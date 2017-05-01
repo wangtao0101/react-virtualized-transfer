b9068b28a437b4389920836133eacf6306d6dea0
yuecjn <yuecjn@gmail.com>
2017-05-01 11:32:42 +0800

    add selectlist for transfer

--- src/transfer.jsx
+++ src/transfer.jsx
@@ -2,7 +2,7 @@ import 'react-virtualized/styles.css';
 import React from 'react';
 import PureRenderMixin from 'rc-util/lib/PureRenderMixin';
 import './transfer.less';
-import Item from './item';
+import SelectList from './selectList';
 
 export default class Transfer extends React.Component {
 
@@ -12,10 +12,32 @@ export default class Transfer extends React.Component {
 
     render() {
         return (
-            <ul>
-                <Item />
-                <Item />
-            </ul>
+            <div>
+                <SelectList
+                    render={item => item.title}
+                    dataSource={this.state.dataSource}
+                    selectKeys={this.state.selectKeys}
+                    handleSelect={this.handleSelect}
+                    footer={this.renderFooter}
+                    showSearch
+                    showHeader
+                    itemsUnit={'items'}
+                    itemUnit={'item'}
+                    titleText={'Source'}
+                />
+                <SelectList
+                    render={item => item.title}
+                    dataSource={this.state.dataSource}
+                    selectKeys={this.state.selectKeys}
+                    // handleSelect={this.handleSelect}
+                    // footer={this.renderFooter}
+                    showSearch
+                    showHeader
+                    itemsUnit={'items'}
+                    itemUnit={'item'}
+                    titleText={'Source'}
+                />
+            </div>
         );
     }
 }
